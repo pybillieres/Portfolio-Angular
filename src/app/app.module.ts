4aed02e495ea93050a136fe35e2dd4f5e7844a60
pybomt@gmail.com <pybomtgmail.com>
2021-02-25 04:03:04 +0100

    Mise en place du routing Création du component home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
+import { ProjectsListComponent } from './home/projects-list/projects-list.component';
+import { EducationComponent } from './home/education/education.component';
+import { SkillsComponent } from './home/skills/skills.component';
+import { ProjectsListItemComponent } from './home/projects-list/projects-list-item/projects-list-item.component';
+import { IntroComponent } from './home/intro/intro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ProjectsListComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsListItemComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
